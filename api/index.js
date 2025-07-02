d17402bac4fdc79eacc4e97ae8c50e16a602aad9
Patricio Horacio Montes Lozano <patricio@192.168.1.7>
2025-07-02 02:08:25 -0600

    debug: Temporary simple handler to test deployment

--- api/index.js
+++ api/index.js
@@ -1,2 +1,15 @@
-// Export the Express app for Vercel
-module.exports = require('../src/index.js');
+// Temporary debug handler
+module.exports = (req, res) => {
+  try {
+    res.status(200).json({ 
+      message: 'Debug test - server is working!',
+      timestamp: new Date().toISOString(),
+      url: req.url,
+      method: req.method
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: 'Debug error: ' + error.message
+    });
+  }
+};
